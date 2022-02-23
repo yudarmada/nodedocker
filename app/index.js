a0c61bf15f15c8d951ad966b2d82859f8aba8c8f
Ludvi Alvin <ludvi@ret.co.id>
2022-02-23 18:04:58 +0700

    update

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World Aasdadsd!')
+  res.send('Hello World YTYUThbashdgahsgd!')
 })
 
 app.listen(port, () => {
